167f6dfa6c4ff283254c7f14dc41f5147d6b9ada
mika101 <mikako1112@gmail.com>
2021-04-01 01:43:30 +0900

    usersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configuration_permitted_parameters, if: :devise_controller?
+
+  private
+  def configuration_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
