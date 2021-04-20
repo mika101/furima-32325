38bc522111aea21e3a918a9b7620a4f1cc5880a5
mika101 <mikako1112@gmail.com>
2021-04-21 02:54:05 +0900

    login画面flash-messeage追加

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,14 @@
+class SessionController < ApplicationController
+  def new
+    @login = Session.new
+  end
+
+  def create 
+    @login = Session.new(email: params[:session][:email], password: params[:session][:password])
+    if @login.save && (@user = User.find_by(email: @login.email)) && @user.authenticate(@login.password)
+      redirect_to users_path
+    else 
+      render :new 
+    end 
+  end 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_163223) do
+ActiveRecord::Schema.define(version: 2021_04_18_085301) do
+
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
