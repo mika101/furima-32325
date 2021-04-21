ba076497e426f4528f1b2fb17a1647924f8f9cf6
mika101 <mikako1112@gmail.com>
2021-04-21 20:39:39 +0900

    修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  with_options presence: true do
+  validates :nickname
   validates :email, uniqueness: true
   validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: '半角英数字混合で入力して下さい' }
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
-  validates :birthday, presence: true
   validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角で入力して下さい' }
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角で入力して下さい' }
   validates :family_name_furigana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' }
   validates :first_name_furigana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' }
+  validates :birthday
+ end
 end
