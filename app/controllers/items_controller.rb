6178bf2d9d00915705a651aad0e47caea65e23eb
mika101 <mikako1112@gmail.com>
2021-04-23 20:02:00 +0900

    修正20

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,5 @@
 class ItemsController < ApplicationController
   def index
   end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to controller: :users, action: :index
-    else
-      render 'new'
-    end
   end
-end
+
