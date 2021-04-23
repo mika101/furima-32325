09470a73abdc6cfae3da2ade11138582dc6644bd
mika101 <mikako1112@gmail.com>
2021-04-23 17:31:09 +0900

    修正16

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
     password = '1a'+Faker::Internet.password(min_length: 6)
-    password = Faker::Internet.password(special_characters: false)
     password { password }
     password_confirmation { password }
     # Fakerは英数字のみ利用可
