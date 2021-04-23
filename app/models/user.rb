9db62e5bfdf1f6a4e6dbde7b25d1f9ffaf4a3528
mika101 <mikako1112@gmail.com>
2021-04-23 17:29:24 +0900

    修正15

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
   validates :nickname
-  validates :email, uniqueness: true
   validates :password,:password_confirmation, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: '半角英数字混合で入力して下さい' }
   validates :birthday
   end
