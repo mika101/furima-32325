cd795298590eac801304733f35877334329a2b25
mika101 <mikako1112@gmail.com>
2021-04-23 16:42:34 +0900

    修正11

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
    resources :items, only: [:new, :create, :show, :edit, :update, :destroy] 
    resources :users, only: [:new, :create, :show, :edit, :update, :destroy] 
    resources :sessions, only: [:new, :create, :show, :update] 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
