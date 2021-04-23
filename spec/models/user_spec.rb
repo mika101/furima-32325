1b97d4eb90dc21028dc7062c65a8a59c57872b37
mika101 <mikako1112@gmail.com>
2021-04-23 15:55:12 +0900

    修正8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,12 @@ RSpec.describe User, type: :model do
           another_user.valid?
           expect(another_user.errors.full_messages).to include('Email has already been taken')
         end
+        it 'emailに@が含まれていない場合登録できないこと' do
+          @user.save
+          @user.email = 'test.com'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Email is invalid')
+        end
         it 'passwordが空では登録できないこと' do
           @user.password = nil
           @user.valid?
