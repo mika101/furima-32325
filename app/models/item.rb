21197fee77c1738915a2f01d2211a294979b4d87
mika101 <mikako1112@gmail.com>
2021-05-02 10:26:14 +0900

    ActiveHashを利用

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,27 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description_of_item 
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :product_condition_id, numericality: { other_than: 1 } 
+    validates :burden_id, numericality: { other_than: 1 } 
+    validates :area_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id, numericality: { other_than: 1 } 
+    validates :price,inclision: { in: 300..9999999 },format: { with: /\A[0-9]+\z/ }
+  end
+
   belongs_to :user
   has_one :purchase
   has_one_attached :image
-  belongs_to :genre
-  validates :title, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
 
-  belongs_to :status
-  validates :title, :text, presence: true
-  validates :status_id, numericality: { other_than: 1 } 
-
+  belongs_to :category
+  #validates :title, :text, presence: true
+  #belongs_to :product_condition
   belongs_to :burden
-  validates :title, :text, presence: true
-  validates :burden_id, numericality: { other_than: 1 } 
-
   belongs_to :area
-  validates :title, :text, presence: true
-  validates :area_id, numericality: { other_than: 1 } 
+  belongs_to :shipping_day
+ 
 
-  belongs_to :shipping
-  validates :title, :text, presence: true
-  validates :shipping_id, numericality: { other_than: 1 } 
 
 end
--- app/models/product_condition.rb
+++ app/models/product_condition.rb
@@ -1,4 +1,4 @@
-class Status < ActiveHash::Base
+class ProductCondition < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '新品、未使用' },
@@ -7,7 +7,7 @@ class Status < ActiveHash::Base
     { id: 5, name: 'やや傷や汚れあり' },
     { id: 6, name: '傷や汚れあり' },
     { id: 7, name: '全体的に状態が悪い' }
- 
+
   ]
 
   include ActiveHash::Associations
