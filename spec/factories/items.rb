5feedf597253488dca49ebf901bd6f2347b702ca
mika101 <mikako1112@gmail.com>
2021-05-03 20:35:44 +0900

    修正7

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     association :user
     item_name { '商品名' }
     description_of_item { '商品の説明' }
-    category_id { '2' }
-    product_condition_id { '2' }
-    price { '9999999' }
-    burden_id { '2' }
-    area_id { '2' }
-    shipping_day_id { '2' }
+    category_id { 2 }
+    product_condition_id { 2 }
+    price { 999999 }
+    burden_id { 2 }
+    area_id { 2 }
+    shipping_day_id { 2 }
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
