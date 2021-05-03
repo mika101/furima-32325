5354e5048bbce27ac809583640e4a969e319ec95
mika101 <mikako1112@gmail.com>
2021-05-03 20:33:18 +0900

    修正6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
-  post 'items/new'
   root to: "items#index"
    resources :items, only: [:index, :show, :new, :create, :show, :edit, :update, :destroy] 
 end
