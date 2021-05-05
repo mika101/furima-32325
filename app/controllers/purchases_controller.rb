1fc0a6844d2a243adcca13bdc7d4ccc02e08566f
mika101 <mikako1112@gmail.com>
2021-05-05 19:21:12 +0900

    修正1

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,22 @@
 class PurchasesController < ApplicationController
-  def new
-    @purchase = Purchase.new
-  end
+  # def new
+    #@purchase = Purchase.new
+  #end
 
-  def create
-    @purchase = Purchase.new(item_params)
-    if @purchase.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
 
-  def show
-    @purchases = Purchase.all
-    @purchases = Purchase.where(id: params[:id])
-  end
+  #def create
+   # @purchase = Purchase.new(item_params)
+    #if @purchase.save
+     # redirect_to root_path
+    #else
+     # render :new
+    #end
+  #end
+
+  #def show
+   # @purchases = Purchase.all
+    #@purchases = Purchase.where(id: params[:id])
+  #end 
+
 end
+
