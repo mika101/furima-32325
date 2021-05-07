ec094287088903a73036f01e946f6901a7d05336
mika101 <mikako1112@gmail.com>
2021-05-07 21:13:39 +0900

    修正6-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def new
     @item = Item.new
   end
@@ -20,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -49,4 +46,8 @@ end
 # 「ログアウト状態で、URLを直接入力して売却済み商品の商品情報編集ページへ遷移しようとすると、ログインページに遷移すること」という機能に関しては、商品購入機能実装後に実装すること
 def move_to_index
   redirect_to action: :index unless user_signed_in?
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
