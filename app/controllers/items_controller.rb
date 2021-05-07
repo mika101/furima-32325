8cbc004d939923f93afd9b23d1df67d010e8c4c9
mika101 <mikako1112@gmail.com>
2021-05-07 21:33:28 +0900

    修正6-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  #before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update]
   def new
     @item = Item.new
@@ -44,9 +44,9 @@ end
 
 # 「ログイン状態で、URLを直接入力して売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移すること」
 # 「ログアウト状態で、URLを直接入力して売却済み商品の商品情報編集ページへ遷移しようとすると、ログインページに遷移すること」という機能に関しては、商品購入機能実装後に実装すること
-def move_to_index
-  redirect_to action: :index unless user_signed_in?
-end
+#def move_to_index
+  #redirect_to action: :index unless user_signed_in?
+#end
 
 def set_item
   @item = Item.find(params[:id])
