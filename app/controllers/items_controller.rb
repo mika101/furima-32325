1b2d320aca2f26a3b9ae011df9905cebbe38ec59
mika101 <mikako1112@gmail.com>
2021-05-08 12:00:23 +0900

    修正7-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :check_id, only: [:edit, :update]
+  before_action :check_id, only: [:edit, :update, :destroy]
 
   def new
     @item = Item.new
