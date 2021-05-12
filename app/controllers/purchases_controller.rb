6976b38d91df5ff695961cb41cf492f1a8874602
mika101 <mikako1112@gmail.com>
2021-05-13 05:29:53 +0900

    決済処理の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,5 @@
 class PurchasesController < ApplicationController
   
-  
    def index
     @purchase_information = PurchaseInformation.new
     @item = Item.find(params[:item_id])
@@ -8,12 +7,13 @@ class PurchasesController < ApplicationController
 
    def create
     #PurchaseInformation.create(purchase_information_params)
-    # binding.pry
+    #  binding.pry
     @item = Item.find(params[:item_id])
     @purchase_information = PurchaseInformation.new(purchase_information_params)
     if @purchase_information.valid?
+      pay_item
       @purchase_information.save
-    redirect_to action: :index
+      redirect_to root_path
     else
     render action: :index
     end
@@ -24,9 +24,16 @@ class PurchasesController < ApplicationController
  private
 
   def purchase_information_params
-        params.require(:purchase_information).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :purchase)
-        .merge(user_id: current_user.id, item_id: params[:item_id])
+        params.require(:purchase_information).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :purchase, :price)
+        .merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+
   end
-
-
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_information_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- app/models/purchase_information.rb
+++ app/models/purchase_information.rb
@@ -1,7 +1,7 @@
 class PurchaseInformation
   
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :purchase_id
+  attr_accessor :item_id, :user_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :purchase_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -10,13 +10,15 @@ class PurchaseInformation
     validates :phone_number, format: { with: /\A\d{11}\z/ }
     validates :user_id
     validates :item_id
+    validates :token
      with_options numericality: { other_than: 1 } do
      validates :area_id
+
      end
   end
   
   def save
-        # ユーザーの情報を保存し、「user」という変数に入れている
+  
     purchase = Purchase.create(item_id:item_id, user_id:user_id)
     ShippingAddress.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
     #Purchase.create(item_id: item.id, user_id: user.id)
