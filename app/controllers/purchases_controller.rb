9a7b700dceee531e68f86fd0e6641423a0620a86
mika101 <mikako1112@gmail.com>
2021-05-22 16:12:41 +0900

    修正8-32

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,10 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item 
+  before_action :set_item
   before_action :move_top_page
 
    def index
+
     @purchase_information = PurchaseInformation.new
   
   end
@@ -35,7 +36,7 @@ class PurchasesController < ApplicationController
     )
   end
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def move_top_page
