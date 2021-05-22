116e002e420e7e5df590f4ff35e214074f9e78b3
mika101 <mikako1112@gmail.com>
2021-05-22 12:31:09 +0900

    修正8-17

--- app/models/purchase_information.rb
+++ app/models/purchase_information.rb
@@ -7,7 +7,7 @@ class PurchaseInformation
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :municipality
     validates :address
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
     validates :token
