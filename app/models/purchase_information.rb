19b3aadb727ed9c0f90cb42e83d87bea1a14680d
mika101 <mikako1112@gmail.com>
2021-05-22 13:46:51 +0900

    修正8-24

--- app/models/purchase_information.rb
+++ app/models/purchase_information.rb
@@ -20,7 +20,6 @@ class PurchaseInformation
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
     ShippingAddress.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address,
                            building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
-    # Purchase.create(item_id: item.id, user_id: user.id)
   end
 end
 
