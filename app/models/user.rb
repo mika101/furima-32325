55cd1eccce4e41903610f4d5363e822f990dc1f6
mika101 <mikako1112@gmail.com>
2021-05-22 13:49:28 +0900

    修正8-26

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :shipping_address
   has_one :purchase
   with_options presence: true do
     validates :nickname
